e6d9b38c6147e310484705e72ee4e87fe0d8ef01
Aleksander Jonczyk <o.jonczyk2@gmail.com>
2022-10-05 17:21:32 +0200

    Adding HTML and styling header

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 // Add more server api links
 export const EUNE_API_LINK = "https://eun1.api.riotgames.com/lol";
+export const SUMMONER_ICONS_LINK =
+  "http://ddragon.leagueoflegends.com/cdn/12.19.1/img/profileicon";
