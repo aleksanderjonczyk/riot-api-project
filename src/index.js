0e48525563323aea1587e5d207f8474b0346d9e9
Aleksander Jonczyk <o.jonczyk2@gmail.com>
2022-10-05 19:07:13 +0200

    Display data on page

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import eun1API from "./riotAPI.js";
 import { KEY } from "./API_KEY.js";
+import { SUMMONER_ICONS_LINK } from "./config.js";
+
+const summonerIcon = document.querySelector(".summoner-icon");
+const summonerName = document.querySelector(".summoner-name");
+const summonerLvl = document.querySelector(".summoner-level");
+const summonerRank = document.querySelector(".summoner-rank");
+const summonerWins = document.querySelector(".summoner-rank-wins");
+const summonerLosses = document.querySelector(".summoner-rank-losses");
+const summonerWinrate = document.querySelector(".summoner-rank-winrate");
+
+const state = {
+  summonerId: null,
+};
 
 const getSummonerByName = async name => {
-  return await eun1API.get(
+  const { data } = await eun1API.get(
     `/summoner/v4/summoners/by-name/${name}?api_key=${KEY}`
   );
+  summonerIcon.style.backgroundImage = `url(${SUMMONER_ICONS_LINK}/${data.profileIconId}.png)`;
+  summonerName.innerHTML = data.name;
+  summonerLvl.innerHTML = data.summonerLevel;
+  getSummonersRank(data.id);
 };
+getSummonerByName("yakaii");
 
 const getSummonersRank = async id => {
-  return await eun1API.get(
+  const { data } = await eun1API.get(
     `/league/v4/entries/by-summoner/${id}?api_key=${KEY}`
   );
+  const { losses, rank, tier, wins } = data[1];
+  summonerRank.innerHTML = `${tier} ${rank}`;
+  summonerWins.innerHTML = wins;
+  summonerLosses.innerHTML = losses;
+  summonerWinrate.innerHTML = Math.round((wins / (wins + losses)) * 100);
 };
-
-const getSummonerData = async name => {
-  const { data } = await getSummonerByName("yakaii");
-  const res = await getSummonersRank(data.id);
-  console.log(res.data);
-};
-
-getSummonerData();
