25fb54e6dc1e9c54ad6c892247f61a7757a08a5f
Aleksander Jonczyk <o.jonczyk2@gmail.com>
2022-10-12 18:29:06 +0200

    Adding search functionality and a bit styling.

--- src/index.js
+++ src/index.js
@@ -2,48 +2,54 @@ import eun1API from "./riotAPI.js";
 import { KEY } from "./API_KEY.js"; // Key expires every 24 hours so ask me for one or create your own on https://developer.riotgames.com
 import { SUMMONER_ICONS_LINK } from "./config.js";
 import { rankingEmblem } from "./rankingEmblem.js";
-
+const summonerHeader = document.querySelector(".summoner__header");
 const summonerIcon = document.querySelector(".summoner-data__icon");
 const summonerName = document.querySelector(".summoner-data__name");
-const summonerLvl = document.querySelector(".summoner-level");
+const summonerLvl = document.querySelector(".summoner-data__level");
 const summonerRank = document.querySelector(".summoner-data-ranking__tier");
 const summonerRankType = document.querySelector(".summoner-data-ranking__type");
 const summonerEmblem = document.querySelector(".summoner-data-ranking__emblem");
 const summonerWins = document.querySelector(".summoner-rank-wins");
 const summonerLosses = document.querySelector(".summoner-rank-losses");
 const summonerWinrate = document.querySelector(".summoner-rank-winrate");
-
-const state = {
-  summonerId: null,
-};
+const searchButton = document.querySelector(".search__button");
+const search = document.querySelector(".search__input");
 
 const getSummonerByName = async name => {
-  const { data } = await eun1API.get(
+  const res = await eun1API.get(
     `/summoner/v4/summoners/by-name/${name}?api_key=${KEY}`
   );
-  if (!data) return;
+  // .catch(() => (summonerHeader.innerHTML = "Summoner does not exist."));
+  if (!res) return;
+  const { data } = res;
+  summonerHeader.innerHTML = "";
   getSummonersRank(data.id);
   summonerIcon.style.backgroundImage = `url(${SUMMONER_ICONS_LINK}/${data.profileIconId}.png)`;
-  summonerIcon.innerHTML = "";
   summonerName.innerHTML = data.name;
-  summonerLvl.innerHTML = data.summonerLevel;
+  summonerLvl.innerHTML = "Level " + data.summonerLevel;
 };
 
 const getSummonersRank = async id => {
   const { data } = await eun1API.get(
     `/league/v4/entries/by-summoner/${id}?api_key=${KEY}`
   );
-  if (!data) return;
-  const { queueType, losses, rank, tier, wins } = data[1];
+  if (!data[0]) return;
+  const { queueType, losses, rank, tier, wins } = data[0];
   summonerRankType.innerHTML = queueType;
   summonerEmblem.style.backgroundImage = `url(${rankingEmblem(tier)})`;
   summonerRank.innerHTML = `${tier} ${rank}`;
-  summonerWins.innerHTML = wins;
-  summonerLosses.innerHTML = losses;
-  summonerWinrate.innerHTML = Math.round((wins / (wins + losses)) * 100);
+  summonerWins.innerHTML = "Wins: " + wins;
+  summonerLosses.innerHTML = "Loses: " + losses;
+  summonerWinrate.innerHTML = `Winrate: ${Math.round(
+    (wins / (wins + losses)) * 100
+  )}%`;
 };
 
-getSummonerByName("yakaii");
+searchButton.addEventListener("click", e => {
+  e.preventDefault();
+  if (!search.value) return;
+  getSummonerByName(search.value);
+  search.value = "";
+});
 
-//Create working summoner search
 //Show all other ranks
