7b309086428258b623dcca13b7afb285e3ca970c
Aleksander Jonczyk <o.jonczyk2@gmail.com>
2022-10-19 00:16:35 +0200

    Display all ranks

--- src/index.js
+++ src/index.js
@@ -6,13 +6,7 @@ const summonerHeader = document.querySelector(".summoner__header");
 const summonerIcon = document.querySelector(".summoner-data__icon");
 const summonerName = document.querySelector(".summoner-data__name");
 const summonerLvl = document.querySelector(".summoner-data__level");
-const summonerRank = document.querySelector(".summoner-data-ranking__tier");
-const summonerLP = document.querySelector(".summoner-data-ranking__lp");
-const summonerRankType = document.querySelector(".summoner-data-ranking__type");
-const summonerEmblem = document.querySelector(".summoner-data-ranking__emblem");
-const summonerWins = document.querySelector(".summoner-rank-wins");
-const summonerLosses = document.querySelector(".summoner-rank-losses");
-const summonerWinrate = document.querySelector(".summoner-rank-winrate");
+const summonerRanking = document.querySelector(".summoner-data-ranking");
 const searchButton = document.querySelector(".search__button");
 const search = document.querySelector(".search__input");
 
@@ -34,24 +28,45 @@ const getSummonersRank = async id => {
   const { data } = await eun1API.get(
     `/league/v4/entries/by-summoner/${id}?api_key=${KEY}`
   );
-  if (!data[0]) return;
-  const { queueType, losses, rank, tier, wins, leaguePoints } = data[0];
-  summonerRankType.innerHTML = queueType;
-  summonerEmblem.style.backgroundImage = `url(${rankingEmblem(tier)})`;
-  summonerRank.innerHTML = `${tier} ${rank}`;
-  summonerLP.innerHTML = "LP: " + leaguePoints;
-  summonerWins.innerHTML = "Wins: " + wins;
-  summonerLosses.innerHTML = "Loses: " + losses;
-  summonerWinrate.innerHTML = `Winrate: ${Math.round(
-    (wins / (wins + losses)) * 100
-  )}%`;
+  if (!data) return;
+
+  data.forEach(queue => {
+    summonerRanking.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <div class="summoner-data-rank">
+      <h6 class="summoner-data-ranking__type">${
+        queue.queueType === "RANKED_SOLO_5x5"
+          ? "Soloqueue"
+          : "Draft Ranked Flex"
+      }</h6>
+    <div class="summoner-data-ranking__emblem" style="background-image: url(${rankingEmblem(
+      queue.tier
+    )})"></div>
+      
+      <div class="summoner-data-ranking__tier">${queue.tier} ${queue.rank}</div>
+      <div class="summoner-data-ranking__lp">LP: ${queue.leaguePoints}</div>
+      <div class="summoner-rank">
+        <span class="summoner-rank-wins">Wins: ${queue.wins}</span>
+        <span class="summoner-rank-losses">Losses: ${queue.losses}</span>
+        <span class="summoner-rank-winrate">Winrate: ${Math.round(
+          (queue.wins / (queue.wins + queue.losses)) * 100
+        )}%</span>
+      </div>
+    </div>
+    </div>
+    `
+    );
+  });
 };
 
 searchButton.addEventListener("click", e => {
   e.preventDefault();
+  summonerRanking.innerHTML = "";
   if (!search.value) return;
   getSummonerByName(search.value);
   search.value = "";
 });
 
-//Show all other ranks
+// TODO:
+// Fix error when you search for summoner that doesnt exist or has rank
